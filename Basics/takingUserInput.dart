f2eb6168ddd1048e6a5dc52d53b6b05cb610ea44
ambivert-sam <samwasu999@gmail.com>
2022-01-29 15:04:46 +0530

    Update takingUserInput.dart

--- Basics/takingUserInput.dart
+++ Basics/takingUserInput.dart
@@ -1,11 +1,11 @@
-import 'dart:io';
+import 'dart:io'; // importing library
 
 void main() {
-  stdout.writeln('Enter your name: ');
+  stdout.writeln('Enter your name: '); //Asking the name as output
 
-  String? name = stdin.readLineSync();
+  String? name = stdin.readLineSync(); //Taking input from user
 
-  print("Your name is $name");
+  print("Your name is $name"); //Priniting name
   // Asking for favourite number
   print("Enter your favourite number:");
 
